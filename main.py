d337926d3abc4aa0e45c4a3e450671e39fcdaa3e
Calamardes <114599889+Calamardes@users.noreply.github.com>
2025-07-10 17:43:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from excel_handler import guardar_fila_excel
+from datetime import date
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"mensaje": "La API está funcionando correctamente"}
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+EXCEL_PATH = "PRUEBA.xlsx"
+EXCEL_SHEET = "protocol"
+
+@app.get("/", response_class=HTMLResponse)
+def formulario(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "mensaje": ""})
+
+@app.post("/registro", response_class=HTMLResponse)
+def registrar(
+    request: Request,
+    protocol_number: str = Form(...),
+    status: str = Form(...),
+    submission_date: str = Form(...),
+    observation_notes: str = Form("")
+):
+    fila = {
+        "protocol_number": protocol_number,
+        "status": status,
+        "submission_date_rp": submission_date,
+        "observation_notes": observation_notes
+    }
+
+    ok, mensaje = guardar_fila_excel(EXCEL_PATH, EXCEL_SHEET, fila)
+    return templates.TemplateResponse("index.html", {"request": request, "mensaje": mensaje})
